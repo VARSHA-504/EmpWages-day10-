ff4a4cffaef65bec2543e048ec6411128ac1a376
varsha-504 <antargangivarsha.com>
2022-03-24 22:52:22 +0530

    added EmpWage Uc3 program

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,15 +3,23 @@ package com.bl.empwages;
 public class EmpWageComputation {
 
     public static void main(String[] args) {
-        int Is_present = 1;
+        int is_present= 1;
+        int is_parttime = 2;
+        int hrs = 0 ;
         int wage = 20;
-        int hrs = 8;
-        int salary = wage*hrs;
-        double empCheck = Math.floor((Math.random()*100)%2);
-        if (empCheck == Is_present)
+        double empCheck = Math.floor((Math.random()*100)%3);
+        if (empCheck == is_present) {
             System.out.println("Employee is Present ");
-        else
+            hrs = 8;
+        }
+        else if (empCheck == is_parttime) {
+            System.out.println("Employee is Part time ");
+            hrs = 4;
+        }
+        else {
             System.out.println("Employee is absent");
-        System.out.println("The daily wage is " + salary);
+        }
+        int salary = wage * hrs;
+        System.out.println("The salary is " + salary);
     }
 }
