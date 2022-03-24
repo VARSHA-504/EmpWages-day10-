392bbb80139e98001e8f869fa1bb4655f8d874f8
varsha-504 <antargangivarsha.com>
2022-03-24 22:44:50 +0530

    added empWage program

---
+++ EmpWageComputation.java
@@ -0,0 +1,8 @@
+package com.bl.empwages;
+
+public class EmpWageComputation {
+
+    public static void main(String[] args) {
+	    System.out.println("Welcome to Employee wage computation");
+    }
+}
