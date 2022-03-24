7bade8a4137832610f4cb0ca460c178b3866bae8
varsha-504 <antargangivarsha.com>
2022-03-24 22:56:05 +0530

    added EmpWage Uc6 program

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,13 +3,16 @@ package com.bl.empwages;
 public class EmpWageComputation {
 
     public static void main(String[] args) {
-        //Variables
         int hrs;
         int wage = 20;
         int total_hrs = 0;
         int salary = 0;
         int maxWorkingDays = 20;
-        for (int days = 1; days < maxWorkingDays; days++) {
+        int total_days = 0;
+        int maxWorkingHrs = 99;
+        //Looping for a month
+        while (total_days < maxWorkingDays && total_hrs <= maxWorkingHrs) {
+            total_days++;
             //Performing check
             double empCheck = Math.floor((Math.random() * 100) % 3);
             //Operations
@@ -21,6 +24,7 @@ public class EmpWageComputation {
             total_hrs += hrs;
             salary = wage * total_hrs;
         }
+        System.out.println("Total Working Days are " +total_days);
         System.out.println("Total Working Hours are " + total_hrs);
         System.out.println("The total salary is " + salary);
     }
