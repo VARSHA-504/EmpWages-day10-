682e7a90b2ce58a5f27677d5b261103a6736a88c
varsha-504 <antargangivarsha.com>
2022-03-24 22:55:09 +0530

    added EmpWage Uc5 program

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,23 +4,24 @@ public class EmpWageComputation {
 
     public static void main(String[] args) {
         //Variables
-        int hrs = 0 ;
+        int hrs;
         int wage = 20;
-        //Performing check
-        double empCheck = Math.floor((Math.random()*100)%3);
-        //Operations
-        switch ((int) empCheck) {
-            case 1 -> {
-                System.out.println("Employee is Present ");
-                hrs = 8;
+        int total_hrs = 0;
+        int salary = 0;
+        int maxWorkingDays = 20;
+        for (int days = 1; days < maxWorkingDays; days++) {
+            //Performing check
+            double empCheck = Math.floor((Math.random() * 100) % 3);
+            //Operations
+            switch ((int) empCheck) {
+                case 1 -> hrs = 8;
+                case 2 -> hrs = 4;
+                default -> hrs = 0;
             }
-            case 2 -> {
-                System.out.println("Employee is Part time ");
-                hrs = 4;
-            }
-            default -> System.out.println("Employee is absent");
+            total_hrs += hrs;
+            salary = wage * total_hrs;
         }
-        int salary = wage * hrs;
-        System.out.println("The salary is " + salary);
+        System.out.println("Total Working Hours are " + total_hrs);
+        System.out.println("The total salary is " + salary);
     }
 }
