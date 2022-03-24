6adfd6b64901ff43458951065aa0dc9fddd7da58
varsha-504 <antargangivarsha.com>
2022-03-24 22:48:38 +0530

    added EmpWage Uc1 program

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,5 +4,11 @@ public class EmpWageComputation {
 
     public static void main(String[] args) {
 	    System.out.println("Welcome to Employee wage computation");
+        int Is_present = 1;
+        double empCheck = Math.floor((Math.random()*100)%2);
+        if (empCheck == Is_present)
+            System.out.println("Employee is Present ");
+        else
+            System.out.println("Employee is absent");
     }
 }
